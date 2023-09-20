0970c61f54d03e82c3fe58234ddc87f277997692
“Dennys” <d.pletikapa@proton.me>
2023-09-20 19:28:39 +0300

    npm run build (from dev) upload

--- js/index.js
+++
@@ -1,2 +0,0 @@
-import './modules/accordion.js';
-import './modules/modal.js';
--- js/theory/script/test.js
+++
@@ -1,5 +0,0 @@
-// document.body.style.backgroundColor = '#000';
-// document.body.style.color = '#fff';
-import './acc.js';
-import './tabs.js';
-
